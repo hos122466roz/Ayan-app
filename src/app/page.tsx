402a60ddf4629ada4658288a8fb44ef2670e3ee5
Hossein Roozdar <hoesin122466@gmail.com>
2025-04-26 23:41:50 +0330

    created app ayan

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import AbouteClient from "./about/AbouteClient";
 import Hero from "./components/hero/Hero";
 import Menu from "./components/menu/Menu";
@@ -6,12 +7,14 @@ import ContactUsClient from "./conectUs/ConectUsClient";
 
 export default function Home() {
   return (
-    <div className="">
-     <Hero/>
-     <Menu/>
-     <AbouteClient/>
-     <Service/>
-     <ContactUsClient/>
-    </div>
+    <Suspense fallback={<>Loading...</>}>
+      <div className="">
+        <Hero />
+        <Menu />
+        <AbouteClient />
+        <Service />
+        <ContactUsClient />
+      </div>
+    </Suspense>
   );
 }
