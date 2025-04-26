40c2d8531b25b8970f61e872f5e5bf2b52346a66
Hossein Roozdar <hoesin122466@gmail.com>
2025-04-27 01:28:28 +0330

    created app ayan

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,11 @@
-import Head from '../components/head/Head';
+import Head from '@/app/components/head/Head';
 import img1 from "../../../public/image/head/shish-kebab-skewers-grilled-vegetables_152520-418.jpg";
 import AbouteClient from './AbouteClient';
 const About=()=>{
-    console.log(typeof img1)
+  
     return (
       <>
-          <Head cover={img1} title='درباره ما'/>
+    <Head cover={img1} title='درباره ما'/>
     <AbouteClient/>
       </>
     );
