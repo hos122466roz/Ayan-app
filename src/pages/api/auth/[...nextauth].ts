20fd2068c58c53267108aaa9afd7cea24ac8d0d3
Hossein Roozdar <hoesin122466@gmail.com>
2026-06-24 01:56:26 +0330

    v8

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,50 +1,58 @@
 import NextAuth, { AuthOptions } from "next-auth";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/app/libs/prismadb";
 import bcrypt from "bcrypt";
 
-import prisma from "@/app/libs/prismadb";
 export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     CredentialsProvider({
       name: "credentials",
       credentials: {
-        email: { label: "email", type: "text" },
-        password: { label: "password", type: "password" },
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
       },
       async authorize(credentials) {
         if (!credentials?.email || !credentials?.password) {
-          throw new Error("Invalid credentials");
+          return null;
         }
+
         const user = await prisma.user.findUnique({
           where: {
             email: credentials.email,
           },
         });
-        if (!user || !user?.hashedPassword) {
-          throw new Error("Invalid credentials");
+
+        if (!user || !user.hashedPassword) {
+          return null;
         }
-        const isCorrectPssword = await bcrypt.compare(
+
+        const isValid = await bcrypt.compare(
           credentials.password,
-          user.hashedPassword
+          user.hashedPassword,
         );
 
-        if (!isCorrectPssword) {
-          throw new Error("Invalid credentails");
+        if (!isValid) {
+          return null;
         }
-        return user;
+
+        return {
+          id: user.id,
+          email: user.email,
+          name: user.name,
+        };
       },
     }),
   ],
-  pages: {
-    signIn: "/",
-  },
-  debug: process.env.NODE_ENV === "development",
   session: {
     strategy: "jwt",
   },
+  pages: {
+    signIn: "/auth/signin",
+  },
   secret: process.env.NEXTAUTH_SECRET,
-  
 };
+
 export default NextAuth(authOptions);
+
